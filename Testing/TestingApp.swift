710e56305cd250281310e8b0eafc521658c5258b
Esteban Calvete <esteban.calvete@gmail.com>
2022-05-02 12:32:05 +0200

    First commit

---
+++ Testing/TestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingApp.swift
+//  Testing
+//
+//  Created by Esteban Calvete Iglesias on 2/5/22.
+//
+
+import SwiftUI
+
+@main
+struct TestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
